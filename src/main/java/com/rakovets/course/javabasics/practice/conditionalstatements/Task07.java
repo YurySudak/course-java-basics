ee35d570fd47e7d1491ba635006eb4ee1b44357d
Yury Sudak <zender@tut.by>
2020-10-05 18:05:47 +0300

    feat: add solution for Task07

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task07.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task07.java
@@ -25,5 +25,8 @@ public class Task07 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        String message = "not a leap year";
+        if (year % 4 == 0 && year % 400 != 0) message = "leap year";
+        System.out.println(message);
     }
 }
