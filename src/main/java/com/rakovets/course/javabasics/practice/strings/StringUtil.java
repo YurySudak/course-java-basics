268bb5f24b4639237452cb0d1d2ebf67ab13aac0
Yury Sudak <zender@tut.by>
2020-10-19 15:34:04 +0300

    feat: task 9 - 15 completed

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.javabasics.practice.strings;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
 
     private String str = null;
@@ -8,36 +11,44 @@ public class StringUtil {
         this.str = str;
     }
 
+    //Task 1
     public String uniteStrings(String str) {
         return this.str.concat(str);
     }
 
+    //Task 2
     public int getIndex(char ch) {
         return str.indexOf(ch);
     }
 
+    //Task 3
     public boolean isEqual(String str) {
         return this.str.equals(str);
     }
 
+    //Task 4
     public String trimUpperCase() {
         str = str.trim();
         return str.toUpperCase();
     }
 
+    //Task 5
     public String subStringFrom10Max23() {
         str = str.substring(10);
         return str.substring(0, 23);
     }
 
+    //Task 6
     public String changeSmile() {
         return str.replace(":(", ":)");
     }
 
+    //Task 7
     public boolean startsAndEndsWith(String str, String word) {
         return str.startsWith(word) && str.endsWith(word);
     }
 
+    //Task 8
     public int numberOfVowels() {
         int num = 0;
         for (char ch: str.toCharArray()) {
@@ -45,6 +56,92 @@ public class StringUtil {
                 num++;
         }
         return num;
+    }
+
+    //Task 9
+    public int numberOfDotsAndCommasAndExclamationMarks() {
+        int num = 0;
+        for (char ch: str.toCharArray()) {
+            if (ch == '.' || ch == ',' || ch == '!')
+                num++;
+        }
+        return num;
+    }
+
+    //Task 10
+    public boolean isPalyndrome() {
+        StringBuffer sb = new StringBuffer(str);
+        for (int i = 0; i < sb.length(); i++) {
+            char ch = sb.charAt(i);
+            if (ch == '.' || ch == ',' || ch == '!' || ch == '?' || ch == '\'' || ch == ' ')
+                sb.deleteCharAt(i);
+        }
+        return (sb.toString().equalsIgnoreCase(sb.reverse().toString()));
+    }
+
+    //Task 11
+    public String[] toArray(int n) {
+        if (str.length() % n != 0) return null;
+        String[] strArray = new String[str.length() / n];
+        int num = 0;
+        for (int i = 0; i < str.length(); i = i + n) {
+            strArray[num] = str.substring(i, i + n);
+            num++;
+        }
+        return strArray;
+    }
+
+    //Task 12
+    public int numberOfWords() {
+        str = str.trim();
+        String[] arr = str.split(" +");
+        int num = 0;
+        for (String s : arr) {
+            if (!s.equals("-")) num++;
+        }
+        return num;
+    }
+
+    //Task 13
+    public String fio() {
+        str = str.trim();
+        String[] arr = str.split(" +");
+        String result = "";
+        for (String s : arr) {
+            result += s.charAt(0) + ".";
+        }
+        return result.toUpperCase();
+    }
+
+    //Task 14
+    public String digits() {
+        char[] arr = str.toCharArray();
+        String result = "";
+        for (char ch : arr) {
+            if (Character.isDigit(ch))
+                result += ch;
+        }
+        return result;
+    }
+
+    //Task 15
+    public String noRepeat(String str) {
+        String result = "";
+        char[] charArr = this.str.toCharArray();
+        for (Character ch : charArr) {
+            String s = ch.toString();
+            Pattern p = Pattern.compile(s);
+            Matcher m = p.matcher(str);
+            if (!m.find()) result += s + " ";
+        }
+        charArr = str.toCharArray();
+        for (Character ch : charArr) {
+            String s = ch.toString();
+            Pattern p = Pattern.compile(s);
+            Matcher m = p.matcher(this.str);
+            if (!m.find()) result += s + " ";
+        }
+        return result.trim();
     }
 
 
--- src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
@@ -3,9 +3,12 @@ package com.rakovets.course.javabasics.practice.strings;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class StringUtilTest {
     StringUtil str = new StringUtil("Hello");
 
+    //Task 1
     @Test
     void uniteStringsTest() {
         Assertions.assertEquals("Hello World", str.uniteStrings(" World"));
@@ -13,6 +16,7 @@ public class StringUtilTest {
         Assertions.assertEquals("Hello!", str.uniteStrings("!"));
     }
 
+    //Task 2
     @Test
     void getIndexTest() {
         Assertions.assertEquals(2, str.getIndex('l'));
@@ -22,6 +26,7 @@ public class StringUtilTest {
         Assertions.assertEquals(4, str.getIndex('o'));
     }
 
+    //Task 3
     @Test
     void isEqualTest() {
         Assertions.assertEquals(true, str.isEqual("Hello"));
@@ -31,6 +36,7 @@ public class StringUtilTest {
         Assertions.assertEquals(false, str.isEqual("Hello!"));
     }
 
+    //Task 4
     @Test
     void trimUpperCaseTest() {
         Assertions.assertEquals("HELLO", str.trimUpperCase());
@@ -42,6 +48,7 @@ public class StringUtilTest {
         Assertions.assertEquals("HELLO", str.trimUpperCase());
     }
 
+    //Task 5
     @Test
     void subStringFrom10Max23Test() {
         str.setStr("0123456789012345678901234567890123456789");
@@ -50,6 +57,7 @@ public class StringUtilTest {
         Assertions.assertEquals("class-based, object-ori", str.subStringFrom10Max23());
     }
 
+    //Task 6
     @Test
     void changeSmileTest() {
         str.setStr("Java :(");
@@ -60,6 +68,7 @@ public class StringUtilTest {
         Assertions.assertEquals(";( Oracle", str.changeSmile());
     }
 
+    //Task 7
     @Test
     void startsAndEndsWithTest() {
         Assertions.assertEquals(true, str.startsAndEndsWith("Hello", "Hello"));
@@ -70,6 +79,7 @@ public class StringUtilTest {
         Assertions.assertEquals(false, str.startsAndEndsWith("Java", "Hello"));
     }
 
+    //Task 8
     @Test
     void numberOfVowelsTest() {
         str.setStr("Hello");
@@ -81,4 +91,97 @@ public class StringUtilTest {
         str.setStr("YAHOO!");
         Assertions.assertEquals(4, str.numberOfVowels());
     }
+
+    //Task 9
+    @Test
+    void numberOfDotsAndCommasAndExclamationMarksTest() {
+        str.setStr("Hello, Java!");
+        Assertions.assertEquals(2, str.numberOfDotsAndCommasAndExclamationMarks());
+        str.setStr("Java.");
+        Assertions.assertEquals(1, str.numberOfDotsAndCommasAndExclamationMarks());
+        str.setStr("..,,!!");
+        Assertions.assertEquals(6, str.numberOfDotsAndCommasAndExclamationMarks());
+        str.setStr("YAHOO!");
+        Assertions.assertEquals(1, str.numberOfDotsAndCommasAndExclamationMarks());
+    }
+
+    //Task 10
+    @Test
+    void isPalyndromeTest() {
+        str.setStr("ABBA");
+        Assertions.assertEquals(true, str.isPalyndrome());
+        str.setStr("Madam");
+        Assertions.assertEquals(true, str.isPalyndrome());
+        str.setStr("Was it a cat I saw?");
+        Assertions.assertEquals(true, str.isPalyndrome());
+        str.setStr("No lemon, no melon.");
+        Assertions.assertEquals(true, str.isPalyndrome());
+        str.setStr("Hell to world");
+        Assertions.assertEquals(false, str.isPalyndrome());
+    }
+
+    //Task 11
+    @Test
+    void toArrayTest() {
+        str.setStr("Hello world ");
+        Assertions.assertEquals(Arrays.asList("Hello ", "world "), Arrays.asList(str.toArray(6)));
+        str.setStr("123456789");
+        Assertions.assertEquals(Arrays.asList("123", "456", "789"), Arrays.asList(str.toArray(3)));
+        str.setStr("Java was originally developed by James Gosling    ");
+        Assertions.assertEquals(Arrays.asList("Java was o", "riginally ", "developed ", "by James G", "osling    "), Arrays.asList(str.toArray(10)));
+        str.setStr("!@#$%^&");
+        Assertions.assertEquals(Arrays.asList("!", "@", "#", "$", "%", "^", "&"), Arrays.asList(str.toArray(1)));
+    }
+
+    //Task 12
+    @Test
+    void numberOfWordsTest() {
+        str.setStr("Java applications!");
+        Assertions.assertEquals(2, str.numberOfWords());
+        str.setStr("  Java coffee, the coffee     from Indonesia.   ");
+        Assertions.assertEquals(6, str.numberOfWords());
+        str.setStr("write once, run anywhere  -  WORA");
+        Assertions.assertEquals(5, str.numberOfWords());
+    }
+
+    //Task 13
+    @Test
+    void fioTest() {
+        str.setStr("Gankovich Yuliya Vladimirovna");
+        Assertions.assertEquals("G.Y.V.", str.fio());
+        str.setStr("goncharuk roman viktorovich");
+        Assertions.assertEquals("G.R.V.", str.fio());
+        str.setStr("ZMACHINSKIY DMITRY IVANOVICH");
+        Assertions.assertEquals("Z.D.I.", str.fio());
+        str.setStr("    MASHKOV    Roman    Igorevich    ");
+        Assertions.assertEquals("M.R.I.", str.fio());
+    }
+
+    //Task 14
+    @Test
+    void digitsTest() {
+        str.setStr("123asd456jkl789");
+        Assertions.assertEquals("123456789", str.digits());
+        str.setStr("   abc1t tt2h hh3a sd   ");
+        Assertions.assertEquals("123", str.digits());
+        str.setStr("---000+++*&^%%$111((()))");
+        Assertions.assertEquals("000111", str.digits());
+    }
+
+    //Task 15
+    @Test
+    void noRepeatTest() {
+        str.setStr("proc");
+        Assertions.assertEquals("p r c i n f", str.noRepeat("info"));
+        str.setStr("12345");
+        Assertions.assertEquals("4 5 6 7 8", str.noRepeat("678123"));
+        str.setStr("123abc");
+        Assertions.assertEquals("c", str.noRepeat("ba321"));
+        str.setStr("qwerty");
+        Assertions.assertEquals("", str.noRepeat("qwerty"));
+        str.setStr("ZXCCV");
+        Assertions.assertEquals("C C", str.noRepeat("VZX"));
+    }
+
+
 }
